3a3a8456d73c5af9a6a2444b02606b5ef72f1d54
wsaid-Avans <w.said@student.avans.nl>
2017-05-26 22:16:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -342,7 +342,7 @@ app.get('/customer/:user/device/', function(request, response) {
 });
 
 app.get('/register/:id', function(request, response) {
-    connection.query('SELECT register_history.order_id, register_history.customer_id, register_history.timestamp, orders.price_total from register_history INNER JOIN orders ON orders.id = register_history.order_id WHERE register_history.register_id=?', [request.params.id], function(err, results, fields) {
+    connection.query('SELECT register_history.order_id, register_history.customer_id, register_history.timestamp, orders.price_total from register_history INNER JOIN orders ON orders.id = register_history.order_id WHERE register_history.register_id=? ORDER BY register_history.timestamp DESC', [request.params.id], function(err, results, fields) {
         if (err) {
             console.log('error: ', err);
             throw err;
